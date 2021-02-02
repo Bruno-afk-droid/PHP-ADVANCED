aeabfb00847f4a965b185b83d8751f18e15bd088
Bruno Hofman <bruno.hofman@student.rocva.nl>
2021-02-02 15:58:19 +0100

    c

---
+++ 4-Functions/taak02/functies-2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function rekenUit($kleur){
+    echo '<h2><span style="color: '.$kleur.' ;text-align:center;">Deze tekst is in de kleur '.$kleur.' </span></h2>';
+}
+
+?>
+
+</body>
+</html>
